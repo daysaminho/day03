dc2a80507c19bfefb94286a87fb9943b686d1963
daysaminho <daysam.moussaoui@epitech.digital>
2024-10-02 14:59:48 +0200

    Create ex_06.js

---
+++ ex_06.js
@@ -0,0 +1,23 @@
+function findAddresses(toFind) {
+    const addresses = [
+        ["45 avenue des tulipes", "92130 Issy-les-Moulineaux"],
+        ["95 avenue parmentier", "75011 Paris"],
+        ["24 Rue Pasteur", "92400 Courbevoie"],
+        ["Champ de Mars, 5 Av. Anatole France", "75007 Paris"],
+        ["Pl. Louis-Armand", "75571 Paris"]
+    ];
+
+    
+    addresses.forEach(address => {
+       
+        const fullAddress = address.join(', ');
+
+        
+        if (fullAddress.includes(toFind)) {
+            console.log(fullAddress);
+        }
+    });
+}
+
+
+findAddresses("Paris");
