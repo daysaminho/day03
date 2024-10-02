d16ac1807e94725b4149156a417519d5b11fa045
daysaminho <daysam.moussaoui@epitech.digital>
2024-10-02 10:22:40 +0200

    Create ex_01.js

---
+++ ex_01.js
@@ -0,0 +1,12 @@
+function getAngryDog(nombreDeWoofs) {
+   
+    let woofString = "";
+    
+ 
+    for (let i = 0; i < nombreDeWoofs; i = i + 1) {
+        woofString += "woof ";
+    }
+ return woofString.trim();
+
+   }
+
